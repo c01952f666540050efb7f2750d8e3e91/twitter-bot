f86930b0d7c6a89266a85566f2474830a5551c3a
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2022-10-02 12:51:48 +0200

    testbed recommit

--- testbed.py
+++ testbed.py
@@ -3,5 +3,20 @@
 
 # I will need to contstruct a authoriseurl
 import requests as req
-
 from twitterDS.endpoints import baseurl, epdata
+from dotenv import load_dotenv
+import os
+
+# Load .env
+load_dotenv()
+bearer = os.getenv("BEARER")
+support_acc_id = os.getenv("SUPPORT_ACC_ID")
+ledger_acc_id = os.getenv("LEDGER_ACC_ID")
+
+agent_key = os.getenv("API_KEY")
+agent_secret = os.getenv("API_SECRET")
+
+headers = {
+    'Content-type': 'application/json', 
+    "Authorization": f"Bearer {bearer}"
+    }
