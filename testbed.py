0a47b896cf2b0c389dafe74d46844104fbfae0cc
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2022-10-03 17:01:18 +0200

    pasted twitter python posting with oauth

--- testbed.py
+++ testbed.py
@@ -6,6 +6,8 @@ import requests as req
 from twitterDS.endpoints import baseurl, epdata
 from dotenv import load_dotenv
 import os
+from requests_oauthlib import OAuth1Session
+import json
 
 # Load .env
 load_dotenv()
@@ -16,7 +18,62 @@ ledger_acc_id = os.getenv("LEDGER_ACC_ID")
 agent_key = os.getenv("API_KEY")
 agent_secret = os.getenv("API_SECRET")
 
-headers = {
-    'Content-type': 'application/json', 
-    "Authorization": f"Bearer {bearer}"
-    }
+# Get request token
+request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+oauth = OAuth1Session(agent_key, client_secret=agent_secret)
+
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print(
+        "There may have been an issue with the consumer_key or consumer_secret you entered."
+    )
+
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key)
+
+# Get authorization
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Please go here and authorize: %s" % authorization_url)
+verifier = input("Paste the PIN here: ")
+
+# Get the access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    agent_key,
+    client_secret=agent_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens["oauth_token"]
+access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# Make the request
+oauth = OAuth1Session(
+    agent_key,
+    client_secret=agent_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# Making the request
+response = oauth.post(
+    "https://api.twitter.com/2/tweets",
+    json={'text': "Testing out the twitter api..."},
+)
+
+if response.status_code != 201:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+
+# Saving the response as JSON
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))
