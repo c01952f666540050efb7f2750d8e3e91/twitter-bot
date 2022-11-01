e80008097c371a9fe296554453e699ef2ced0777
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2022-11-01 10:16:07 +0100

    implementing more PIN input

--- botFrontend/app.py
+++ botFrontend/app.py
@@ -1,8 +1,28 @@
-from ctypes.wintypes import PINT
-from dash import Dash, dcc, html, Input, Output, State, dcc
+# Dash imports
+from dash import Dash, dcc, html, Input, Output, State, dcc, ctx
 import dash_bootstrap_components as dbc
 from dash_bootstrap_components._components.Container import Container
-import base64
+
+# General Imports
+import os
+from ctypes.wintypes import PINT
+import requests as req
+from dotenv import load_dotenv
+import os
+from requests_oauthlib import OAuth1Session
+import json
+
+# Internal imports
+from components import *
+
+# Load .env
+load_dotenv(dotenv_path="../")
+bearer = os.getenv("BEARER")
+support_acc_id = os.getenv("SUPPORT_ACC_ID")
+ledger_acc_id = os.getenv("LEDGER_ACC_ID")
+
+agent_key = os.getenv("API_KEY")
+agent_secret = os.getenv("API_SECRET")
 
 # How we want this to work:
 # We have a link in the navbar top right - asking if you want to login
@@ -11,121 +31,38 @@ import base64
 # once you've done that, the pin input dissappears and you will be navigated to
 # the twitter dashboard accordingly
 
-
 # Added dark theme
 app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
-# Ledger Logo
-logo_fp = r"assets/ledger_logo.jpg"
-encoded_image = base64.b64encode(open(logo_fp, 'rb').read())
-
-
-# Search Bar
-pin_bar = dbc.Row(
+@app.callback(
+    Output(component_id="main-window", component_property="children"),
     [
-        dbc.Col(dbc.Input(type="password", placeholder="Enter PIN")),
-        dbc.Col(
-            dbc.Button(
-                "Search", color="primary", className="ms-2", n_clicks=0
-            ),
-            width="auto",
-        ),
-    ],
-    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
-    align="center",
+        Input("pin-request", "n_clicks")
+    ]
 )
+def contentWindowManager(clickNumber):
+    msg = "button not pressed"
+    if "pin-request" == ctx.triggered_id:
 
-# Nav Bar
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src=logo_fp, height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Ledger Protect Twitter Bot Frontend (v0.01)", className="ms-2")),
-                    ],
-                    align="center",
-                    className="g-0",
-                ),
-                href="http://localhost:8050",
-                style={"textDecoration": "none"},
-            ),
-            # Update the below so it only shows when required - we will need to get the state of the oauth login somehow
-            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
-            dbc.NavItem(dbc.NavLink("Get PIN", href="#")),
-            dbc.Collapse(
-                pin_bar,
-                id="navbar-collapse",
-                is_open=False,
-                navbar=True,
-            ),
-        ]
-    ),
-    color="dark",
-    dark=True
-)
+        request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+        oauth = OAuth1Session(agent_key, client_secret=agent_secret)
 
-# add callback for toggling the collapse on small screens
-@app.callback(
-    Output("navbar-collapse", "is_open"),
-    [Input("navbar-toggler", "n_clicks")],
-    [State("navbar-collapse", "is_open")],
-)
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
+        msg = f"button pressed {clickNumber} times"
+    return html.Div(msg)
 
-# button callback
-@app.callback(
-    Output('app2', 'children'),
-    Input('pin-code', 'value')
-)
-def update_output(value):
-    return [f'The input value was {value}']
-
-
-# Current input DIV
-app.layout = html.Div([
-    navbar,
-    html.Div([
-        "Input: ",
-        dcc.Input(
-            id="pin-code", 
-            placeholder="PIN Code", 
-            type="password"
-            )
-    ]),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Br(),
-    html.Div(id="app"),
-    html.Div(id="app2")
-])
-
-# Current SHOW PIN div - testing
-@app.callback(
-    Output(component_id="app", component_property="children"),
-    Input(component_id="pin-code", component_property="value")
-)
-def providePinCode(inputValue):
-    # function that will return the PIN Code
-    # Think about processing this within function?
-    return f'Pin Code {inputValue}'
-
-# @app.callback(
-#     Output(component_id="oauth-link", component_property="children"),
-#     Input(component_id="")
-# )
-# def testCallback():
-#     return None
 
 # We will want to make multiple callbacks
 # The first will be for authentication and to begin running the bot
 # We will want to know how close we are to the rate limit as well as
 # Who we're responding to
 # It will be nice to know what kind of scams are out there as well
+
+# Main app layout
+app.layout = html.Div([
+    navbar,
+    html.Br(),
+    html.Div(id="main-window")
+])
 
 # To Run, use below:
 app.run_server(debug=True)
--- botFrontend/components.py
+++ botFrontend/components.py
@@ -1,1 +1,67 @@
+# Dash Imports
 from dash import Dash, dcc, html, Input, Output, dcc
+import dash_bootstrap_components as dbc
+from dash_bootstrap_components._components.Container import Container
+
+# Image Imports
+import base64
+
+# Ledger Logo
+logo_fp = r"assets/ledger_logo.jpg"
+encoded_image = base64.b64encode(open(logo_fp, 'rb').read())
+
+# All the main components to be imported into app.py
+# Enter PIN object
+pin_bar = dbc.Row(
+    [
+        dbc.Col(dbc.Input(
+            id="pin-code",
+            type="password", 
+            placeholder="Enter PIN"
+            )),
+        dbc.Col(
+            dbc.Button(
+                "Submit", color="primary", className="ms-2", n_clicks=0
+            ),
+            width="auto",
+        ),
+    ],
+    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
+    align="center",
+)
+
+pin_request = dbc.Button(
+    "Get PIN",
+    id="pin-request",
+    color="primary",
+    className="ms-2",
+    n_clicks=0
+)
+
+# Nav Bar
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=logo_fp, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Ledger Protect Twitter Bot Frontend (v0.01)", className="ms-2")),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+                href="http://localhost:8050", # Go back to home screen
+                style={"textDecoration": "none"},
+            ),
+            # Update the below so it only shows when required - we will need to get the state of the oauth login somehow
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            pin_request,  # Use this as link to get PIN on mainscreen
+            pin_bar
+        ]
+    ),
+    color="dark",
+    dark=True
+)
+
